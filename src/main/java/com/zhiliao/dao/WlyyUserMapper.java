827f57f0f47cfaf9c69a44ad82bf6391b5b507e8
zhong1024 <mr.zhong@guodasha.cn>
2019-08-12 18:07:16 +0800

    mybatis添加user配置属性01

---
+++ src/main/java/com/zhiliao/dao/WlyyUserMapper.java
@@ -0,0 +1,37 @@
+package com.zhiliao.dao;
+
+import com.zhiliao.pojo.WlyyUser;
+import com.zhiliao.pojo.WlyyUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * 用户mapper
+ *
+ * @author Mr.Zhong
+ * @create2019-08-01 14:13
+ */
+
+public interface WlyyUserMapper {
+    int countByExample(WlyyUserExample example);
+
+    int deleteByExample(WlyyUserExample example);
+
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(WlyyUser record);
+
+    int insertSelective(WlyyUser record);
+
+    List<WlyyUser> selectByExample(WlyyUserExample example);
+
+    WlyyUser selectByPrimaryKey(Integer id);
+
+    int updateByExampleSelective(@Param("record") WlyyUser record, @Param("example") WlyyUserExample example);
+
+    int updateByExample(@Param("record") WlyyUser record, @Param("example") WlyyUserExample example);
+
+    int updateByPrimaryKeySelective(WlyyUser record);
+
+    int updateByPrimaryKey(WlyyUser record);
+}
---
+++ src/main/java/com/zhiliao/pojo/WlyyUser.java
@@ -0,0 +1,40 @@
+package com.zhiliao.pojo;
+
+/**
+ * 用户信息pojo
+ *
+ * @author Mr.Zhong
+ * @create2019-08-01 14:13
+ */
+
+public class WlyyUser {
+    private Integer id;
+
+    private String name;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+}
